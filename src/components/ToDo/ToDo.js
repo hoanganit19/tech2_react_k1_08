1d2d71078056899234a9ba6aabf4ba508f0ec46b
Hoang An Unicode <hoanganit19@gmail.com>
2022-09-27 12:15:07 +0700

    Update code

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { todoSelector, addToDo, removeToDo } from "./toDoSlice";
+import { todoSelector, addToDo, removeToDo, completeToDo } from "./toDoSlice";
 import { v4 as uuidv4 } from "uuid";
+import './ToDo.css';
 
 export default function ToDo() {
   const dispatch = useDispatch();
@@ -18,6 +19,7 @@ export default function ToDo() {
         addToDo({
           id: uuidv4(),
           name: jobName,
+          complete: false
         })
       );
 
@@ -35,6 +37,13 @@ export default function ToDo() {
     dispatch(removeToDo(index));
   };
 
+  const handleComplete = (index, isChecked) => {
+    dispatch(completeToDo({
+      index: index,
+      complete: isChecked
+    }));
+  }
+
   return (
     <div style={{ margin: "3%" }}>
       <div>
@@ -50,11 +59,13 @@ export default function ToDo() {
         </form>
       </div>
       <div>
-        {jobList.map(({ id, name }, index) => (
+        {jobList.map(({ id, name, complete }, index) => (
           <p key={id}>
-            <input type={"checkbox"} />
-            {name}
-            <span
+            <input type={"checkbox"} onChange={(e) => {
+              handleComplete(index, e.target.checked);
+            }}/>
+            <span className={complete?'complete':''}>{name}</span>
+            <span className="remove"
               onClick={() => {
                 handleRemove(index);
               }}
--- src/components/ToDo/toDoSlice.js
+++ src/components/ToDo/toDoSlice.js
@@ -1,25 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const init = {
-    jobs: []
-}
+  jobs: [],
+};
 
 const toDoSlice = createSlice({
-    name: 'todo2',
-    initialState: init,
-    reducers: {
-        add: (state, action) => { //=> todo/add
-            state.jobs.push(action.payload)
-        },
+  name: "todo2",
+  initialState: init,
+  reducers: {
+    add: (state, action) => {
+      //=> todo/add
+      state.jobs.push(action.payload);
+    },
 
-        remove: (state, action) => {
-            state.jobs.splice(action.payload, 1);
-        }
-    }
-})
+    remove: (state, action) => {
+      state.jobs.splice(action.payload, 1);
+    },
 
-export const todoSelector = state => state.todo.jobs; //Tạo selector
+    complete: (state, action) => {
+      state.jobs[action.payload.index].complete = action.payload.complete;
+    },
+  },
+});
 
-export const {add: addToDo, remove: removeToDo} = toDoSlice.actions;
+export const todoSelector = (state) => state.todo.jobs; //Tạo selector
+
+export const {
+  add: addToDo,
+  remove: removeToDo,
+  complete: completeToDo,
+} = toDoSlice.actions;
 
 export default toDoSlice.reducer; //Bắt buộc phải export reducer để store sử dụng
